cc386b4d36ddd1e0e4e6ccb699d13fc2d9fb8465
Finn Kollstad <finnkollstad@gmail.com>
2025-09-15 15:17:09 +0200

    la til noe funksjonalitet

--- config/scribe.php
+++ config/scribe.php
@@ -1,12 +1,14 @@
 <?php
 
 use Knuckles\Scribe\Extracting\Strategies;
-use Knuckles\Scribe\Config\Defaults;
 use Knuckles\Scribe\Config\AuthIn;
 use function Knuckles\Scribe\Config\{removeStrategies, configureStrategy};
 
 // Only the most common configs are shown. See the https://scribe.knuckles.wtf/laravel/reference/config for all.
-
+if (!class_exists(\Knuckles\Scribe\ScribeServiceProvider::class)) {
+    return [];
+}
+use Knuckles\Scribe\Config\Defaults;
 return [
     // The HTML <title> for the generated documentation.
 
